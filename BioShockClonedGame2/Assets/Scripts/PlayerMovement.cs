07b4ca7e661547677fe2fec7e9f08c8c7f46d21e
Max <maxkruger321@gmail.com>
2023-08-22 13:27:18 +0200

    finished walkking and jumping with gravity

--- BioShockClonedGame2/Assets/Scripts/PlayerMovement.cs
+++ BioShockClonedGame2/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-    public float speed = 12f;
+    public float speed = 9f;
 
     Vector3 velocity;
     public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    private bool lerpCrouch;
+    private bool crouching;
+    public float crouchTimer;
+
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +31,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -27,8 +47,40 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt( jumpHeight * -2f * gravity);
+        }
+
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrounded)
+        {
+            speed = 4f;
+        } else if (Input.GetKeyUp(KeyCode.LeftControl) && isGrounded)
+        {
+            speed = 9f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+        {
+            
+                crouching = true;
+                
+                controller.height = 2;
+            
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            crouching = false;
+
+            controller.height = Mathf.Lerp(controller.height, 3, 0.5f);
+        }
+
     }
+
+   
 }
